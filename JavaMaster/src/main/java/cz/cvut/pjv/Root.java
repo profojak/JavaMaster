e8bd5b8c8de5e300e4db1fbc8aeb29f0be2d9341
Jakub Profota <profojak@fel.cvut.cz>
2020-04-04 13:08:23 +0200

    Method structure added

--- JavaMaster/src/main/java/cz/cvut/pjv/Root.java
+++ JavaMaster/src/main/java/cz/cvut/pjv/Root.java
@@ -4,7 +4,20 @@ import cz.cvut.pjv.game.Game;
 
 public class Root {
   /*
-   * This method is entered when the game launches. Exiting this method quits the game.
+   * Listens for a keypress.
+   */
+  public void listen() {
+    // While loop should be present here.
+  }
+
+  /*
+   * Switches between MainMenu, Game and Editor.
+   */
+  public void switchMode() {
+  }
+
+  /*
+   * Entered when the game launches. Exiting this method quits the game.
    */
   public static void main(String args[]) {
     System.out.println("Half-Life 3 would make more money than Steam. Change my mind.");
--- JavaMaster/src/main/java/cz/cvut/pjv/game/Game.java
+++ JavaMaster/src/main/java/cz/cvut/pjv/game/Game.java
@@ -1,5 +1,45 @@
 package cz.cvut.pjv.JavaMaster;
 
 public class Game {
+  public Game() {
+  }
+
+  // Key methods
+  
+  /*
+   * Called when up key is pressed.
+   */
+  public void keyUp() {
+  }
+
+  /*
+   * Called when down key is pressed.
+   */
+  public void keyDown() {
+  }
+
+  /*
+   * Called when left key is pressed.
+   */
+  public void keyLeft() {
+  }
+
+  /*
+   * Called when right key is pressed.
+   */
+  public void keyRight() {
+  }
+
+  /*
+   * Called when escape key is pressed.
+   */
+  public void keyEscape() {
+  }
+
+  /*
+   * Called when enter key is pressed.
+   */
+  public void keyEnter() {
+  }
 }
 
